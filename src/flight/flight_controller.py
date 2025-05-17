9732e57ab603305913f31908065a3a84f055de14
Bryce Wooster <40742705+BryceWDesign@users.noreply.github.com>
2025-05-17 11:43:36 -0700

    Add core methods to FlightController: take_off, land, stabilization, and thruster control to pass initial tests

--- src/flight/flight_controller.py
+++ src/flight/flight_controller.py
@@ -1,11 +1,40 @@
-# src/flight/flight_controller.py
-
 class FlightController:
     def __init__(self):
-        self.status = "initialized"
+        self.is_flying = False
+        self.thrusters = {
+            'main': 0,
+            'left': 0,
+            'right': 0,
+            'rear': 0,
+            'front': 0
+        }
+        self.stabilization_data = {
+            'pitch': 0.0,
+            'roll': 0.0,
+            'yaw': 0.0
+        }
 
-    def start(self):
-        self.status = "in-flight"
+    def take_off(self):
+        self.is_flying = True
+        self.set_thrusters({'main': 100})  # initial upward thrust
 
-    def stop(self):
-        self.status = "stopped"
+    def land(self):
+        self.set_thrusters({'main': 0})
+        self.is_flying = False
+
+    def update_stabilization(self, pitch, roll, yaw):
+        self.stabilization_data['pitch'] = pitch
+        self.stabilization_data['roll'] = roll
+        self.stabilization_data['yaw'] = yaw
+
+    def set_thrusters(self, thrust_dict):
+        for k, v in thrust_dict.items():
+            if k in self.thrusters:
+                self.thrusters[k] = v
+
+    def get_status(self):
+        return {
+            'is_flying': self.is_flying,
+            'thrusters': self.thrusters,
+            'stabilization': self.stabilization_data
+        }
