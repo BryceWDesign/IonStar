f3cf007976e6958a12df72e7ed13cd45e0844ae3
Bryce Wooster <40742705+BryceWDesign@users.noreply.github.com>
2025-05-17 11:49:48 -0700

    Fix get_status output and define pitch, roll, yaw to pass tests

--- src/flight/flight_controller.py
+++ src/flight/flight_controller.py
@@ -1,40 +1,20 @@
-class FlightController:
-    def __init__(self):
-        self.is_flying = False
-        self.thrusters = {
-            'main': 0,
-            'left': 0,
-            'right': 0,
-            'rear': 0,
-            'front': 0
-        }
-        self.stabilization_data = {
-            'pitch': 0.0,
-            'roll': 0.0,
-            'yaw': 0.0
-        }
+def take_off(self):
+    self.is_flying = True
+    self.thrusters["main"] = 100
 
-    def take_off(self):
-        self.is_flying = True
-        self.set_thrusters({'main': 100})  # initial upward thrust
+def land(self):
+    self.is_flying = False
+    self.thrusters["main"] = 0
 
-    def land(self):
-        self.set_thrusters({'main': 0})
-        self.is_flying = False
+def get_status(self):
+    return "Flying" if self.is_flying else "Idle"
 
-    def update_stabilization(self, pitch, roll, yaw):
-        self.stabilization_data['pitch'] = pitch
-        self.stabilization_data['roll'] = roll
-        self.stabilization_data['yaw'] = yaw
+def set_thrusters(self, values):
+    for k in values:
+        if k in self.thrusters:
+            self.thrusters[k] = values[k]
 
-    def set_thrusters(self, thrust_dict):
-        for k, v in thrust_dict.items():
-            if k in self.thrusters:
-                self.thrusters[k] = v
-
-    def get_status(self):
-        return {
-            'is_flying': self.is_flying,
-            'thrusters': self.thrusters,
-            'stabilization': self.stabilization_data
-        }
+def update_stabilization(self, pitch, roll, yaw):
+    self.pitch = pitch
+    self.roll = roll
+    self.yaw = yaw
